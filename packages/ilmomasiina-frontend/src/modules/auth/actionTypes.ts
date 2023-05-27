290edb898f132e459f11b87b7dd79263da3f65dc
Kalle Ahlström <71292737+kqlski@users.noreply.github.com>
2023-05-27 04:45:27 -0700

    remove unused code
    Co-authored-by: PurkkaKoodari <8439661+PurkkaKoodari@users.noreply.github.com>

--- packages/ilmomasiina-frontend/src/modules/auth/actionTypes.ts
+++ packages/ilmomasiina-frontend/src/modules/auth/actionTypes.ts
@@ -2,4 +2,3 @@ export const LOGGING_IN = 'auth/LOGGING_IN';
 export const LOGIN_SUCCEEDED = 'auth/LOGIN_SUCCEEDED';
 export const LOGIN_FAILED = 'auth/LOGIN_FAILED';
 export const RESET = 'auth/RESET';
-export const RENEWING_TOKEN = 'auth/RENEWING_TOKEN';
